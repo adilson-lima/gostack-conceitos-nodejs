08cf3a81735c09a9cf772573c8234386b89a5591
Adilson Pereira Lima <adilson.pereira.lima@gmail.com>
2020-10-28 19:06:13 -0300

    Desafio finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,97 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body
+
+  let repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  // console.log(request.params)
+
+  repositories.push(repository)
+
+  response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const { id } = request.params
+
+  let repositoryIndex = repositories.findIndex(repository => repository.id.includes(id))
+
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'Invalid UUID'})
+  }
+
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: 'Id does not exist'})
+  }
+
+  let repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  //console.log(repositoryIndex)
+
+  repositories[repositoryIndex] = repository
+
+  response.json(repository)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  if(!isUuid(id)){
+    response.status(400).json({error: "UUID is not valid"})
+  }
+
+  let repositoryIndex = repositories.findIndex(repository => repository.id.includes(id))
+
+  if(repositoryIndex < 0){
+    response.status(400).json({error: 'Id does not exist'})
+  }
+
+  repositories.splice(repositoryIndex, 1)
+  
+  response.status(204).json()
+
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params
+
+  if(!isUuid(id)){
+    response.status(400).json({error: "UUID is not valid"})
+  }
+
+  let repositoryIndex = repositories.findIndex(repository => repository.id.includes(id))
+
+  if(repositoryIndex < 0){
+    response.status(400).json({error: 'Id does not exist'})
+  }
+
+  let repository = repositories[repositoryIndex]
+  repository.likes ++
+
+  response.json(repository)
+
 });
 
 module.exports = app;
